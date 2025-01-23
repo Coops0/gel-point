0c462e5fc2526227dd9cf86a47e4c9f0191b112a
Coops0 <51346812+Coops0@users.noreply.github.com>
2025-01-23 09:24:22 -0500

    buy selector looks fucking great

--- app/src/components/PuzzleGrid.vue
+++ app/src/components/PuzzleGrid.vue
@@ -4,7 +4,6 @@
         v-for="(row, rowIndex) in localGrid"
         :key="rowIndex"
         class="flex flex-row gap-2"
-        :class="{'bg-colors-secondary-100': buyMode && selectedRow === rowIndex}"
     >
       <div
           v-for="(cell, colIndex) in row"
@@ -15,7 +14,7 @@
             'bg-colors-secondary-400 text-colors-background-50': cell !== 0 && cell !== -1,
             'bg-colors-secondary-200': cell === 0,
             'invisible': cell === -1,
-            'bg-colors-secondary-100': cell !== -1 && buyMode && selectedCol === colIndex
+            'ring-2 ring-colors-accent-400': cell !== -1 && buyMode && (selectedCol === colIndex || selectedRow === rowIndex)
           }"
       >
         <span v-if="cell !== 0 && cell !== -1">{{ cell }}</span>
@@ -38,11 +37,11 @@ const emit = defineEmits<{
 }>();
 
 const localGrid = ref<Grid>(structuredClone(toRaw(props.grid)));
+
 const selectedRow = ref(-1);
 const selectedCol = ref(-1);
-const lastClickTime = ref(0);
 
-const DOUBLE_CLICK_THRESHOLD = 300;
+let lastClickedCell: [number, number] = [-1, -1];
 
 watch(() => props.grid, updateGrid, { deep: true });
 watch(() => props.buyMode, resetSelection);
@@ -91,11 +90,8 @@ const hasEmptyCells = (rowIndex: number, colIndex: number): [boolean, boolean] =
 function handleCellClick(rowIndex: number, colIndex: number) {
   if (!props.buyMode) return;
 
-  const now = Date.now();
-  const isDoubleClick = (now - lastClickTime.value) < DOUBLE_CLICK_THRESHOLD;
-  lastClickTime.value = now;
-
   const [hasEmptyInRow, hasEmptyInCol] = hasEmptyCells(rowIndex, colIndex);
+  const isDoubleClick = lastClickedCell[0] === rowIndex && lastClickedCell[1] === colIndex;
 
   if (isDoubleClick) {
     // dobule click => try to swap row/col
@@ -122,6 +118,8 @@ function handleCellClick(rowIndex: number, colIndex: number) {
       selectedCol.value = colIndex;
     }
   }
+
+  lastClickedCell = [rowIndex, colIndex];
 
   if (selectedRow.value !== -1 || selectedCol.value !== -1) {
     emit('selected', selectedRow.value, selectedCol.value);
