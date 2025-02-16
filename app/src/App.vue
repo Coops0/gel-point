c08c6cf933e431e2b96601faceac050afb411ce2
Coops0 <51346812+Coops0@users.noreply.github.com>
2025-02-16 12:08:51 -0500

    fix non click a

--- app/src/App.vue
+++ app/src/App.vue
@@ -77,7 +77,8 @@
           <FadeTransition appear>
             <div
                 v-if="showHelp"
-                class="fixed top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-9 bg-primary-900 text-white p-2 rounded-md font-shippori w-fit !pointer-events-none"
+                class="fixed top-3/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-9 bg-primary-900 text-white p-2 rounded-md font-shippori w-fit"
+                :class="!showHelp && '!pointer-events-none'"
                 @click="() => showHelp = false">
               hold and drag letters to build words
             </div>
