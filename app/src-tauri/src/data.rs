97e13da9c750ecceaaf157ff8be7a471d699c7ba
Coops0 <51346812+Coops0@users.noreply.github.com>
2025-02-26 17:50:16 -0500

    Switch to `include-flate` for compressed asset inclusion
    Replaced `include_str!` with `include-flate` to include compressed asset files, reducing memory usage and improving application efficiency. Updated `Cargo.toml` and `Cargo.lock` to include the `include-flate` dependency and its related packages.

--- app/src-tauri/src/data.rs
+++ app/src-tauri/src/data.rs
@@ -1,2 +1,4 @@
-pub const PUZZLES_DATA: &str = include_str!("../assets/puzzles.data");
-pub const WORDS_DATA: &str = include_str!("../assets/words.data");
+use include_flate::flate;
+
+flate!(pub static PUZZLES_DATA: str from "./assets/puzzles.data");
+flate!(pub static WORDS_DATA: str from "./assets/words.data");
